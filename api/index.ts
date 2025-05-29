e0528e3aa23a307eb8e1dcb5f3168f4fa1f6c04c
Andres Uriel Aguirre Ocampo <andres.aguirre_o@hotmail.com>
2025-05-29 15:31:37 -0600

    Fix error

--- api/index.ts
+++ api/index.ts
@@ -1,27 +1,23 @@
 import express from "express";
 import usersRoutes from "./routes/user.router";
 import authRoutes from "./routes/auth.router";
-import { initializeSystem } from "./utils/init";
+import { initializeSystem } from "./utils/init"
 
 const app = express();
+
 app.use(express.json());
 
-initializeSystem()
-  .then(() => {
-    console.log("System initialization complete.");
 
-    app.get("/", (req, res) => {
-      res.send("Express on Vercel with TS");
-    });
+app.get("/", (req, res) => {
+  res.send("Express on Vercel with TS");
+});
 
-    app.use("/users", usersRoutes);
-    app.use("/auth", authRoutes);
+app.get("/initialize", async (req, res) => {
+  await initializeSystem()
+  res.send("app initialized");
+});
 
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("Failed to initialize system:", err);
-  });
+app.use("/users", usersRoutes);
+app.use("/auth", authRoutes);
+
+export default app;
